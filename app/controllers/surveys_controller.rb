924ccdfc707cb3f0f07b8e9821a414c2c992f1fa
Petya Nikolova <petia_182@yahoo.com>
2016-11-21 16:38:39 +0000

    new and create actions added + simple form for new survey added

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -6,9 +6,17 @@ class SurveysController < ApplicationController
   end
 
   def new
+    @survey = Survey.new
   end
 
   def create
+    @survey = Survey.new(survey_params)
+    @survey.user = current_user
+     if @survey.save
+      redirect_to survey_path(@survey)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +27,12 @@ class SurveysController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def survey_params
+    params.require.(:survey).permit(:city, :start_date)
+  end
+
+
 end
