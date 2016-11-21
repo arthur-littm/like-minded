5940e9f710119afb881a57bd37755b85cbfb958e
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-21 18:20:23 +0000

    create survey still not working

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: 'surveys#new'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
