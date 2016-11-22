fabbb87e98cd277191a59bf77ccf6dc6b5c3fa28
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-22 17:33:54 +0000

    sign in design improved

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161122163335) do
+ActiveRecord::Schema.define(version: 20161122161954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,11 +29,6 @@ ActiveRecord::Schema.define(version: 20161122163335) do
     t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_questions_on_category_id", using: :btree
     t.index ["user_id"], name: "index_questions_on_user_id", using: :btree
-  end
-
-  create_table "survey_friends", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "survey_questions", force: :cascade do |t|
