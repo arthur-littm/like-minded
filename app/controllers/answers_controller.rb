2eff9044bce23e1f7ce26e17553dffa7dcbc2ad1
Hunter Gregory <hdg678@gmail.com>
2016-11-23 14:31:53 +0000

    adding small edit

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,8 +5,15 @@ class AnswersController < ApplicationController
   end
 
   def new
+    @answer = Answer.new
   end
 
   def create
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:user_id, :question_id, :survey_id)
   end
 end
