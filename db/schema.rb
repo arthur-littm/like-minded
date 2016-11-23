778d9dd5ff47f13e000bff1b4177027cc2253226
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-23 14:14:51 +0000

    added icon names to categories and icon png to assets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122170225) do
+ActiveRecord::Schema.define(version: 20161123135919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161122170225) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "icon_name"
   end
 
   create_table "questions", force: :cascade do |t|
