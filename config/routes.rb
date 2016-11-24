479b208011cf5a3bde1b8fca1fb70c602a516897
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-24 17:35:21 +0000

    rendering and status working

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,21 @@ Rails.application.routes.draw do
    resources :questions, only: [:index] do
     resources :answers, only: [:show, :new, :create]
   end
-   resources :survey_questions, only: [:destroy]
-   resources :survey_friends, only: [:destroy]
-   member do
+  resources :survey_questions, only: [:destroy]
+  resources :survey_friends, only: [:destroy]
+  member do
     get '/answering', to: 'surveys#answering', as: :survey_answer
-    patch '/update', to: 'surveys#update_friends', as: :update_friends
-   end
+    patch '/update_status', to: 'surveys#update_friends', as: :update_friends
+    patch '/update', to: 'surveys#update_status', as: :update_status
   end
-  resources :questions
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: [:edit, :update]
-  resources :answers, only: [:show, :new, :create]
+end
+resources :questions
+devise_for :users,
+controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+resources :users, only: [:edit, :update]
+resources :answers, only: [:show, :new, :create]
 
-  root to: 'surveys#new'
-  get '/dashboard', to: 'pages#dashboard'
+root to: 'surveys#new'
+get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
