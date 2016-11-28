1fc56973fac7714575f76d85870522be5ec491ce
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-28 09:31:31 +0000

    cards fixed and started doing a page for recieved surveys to answer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def sent_surveys
+    @user = current_user
+    @surveys = Survey.find(friends: current_user)
+  end
+  helper_method :find_sent_surveys
+
   private
 
   def user_params
