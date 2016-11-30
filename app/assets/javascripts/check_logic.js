8ab2e72e2fa2c0da8e47d6a153a653c77cb72a5d
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-30 09:41:33 +0000

    design improved

--- app/assets/javascripts/check_logic.js
+++ app/assets/javascripts/check_logic.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
   $(".checkbox-trigger").click(function(){
-    $(this).parent().find(".fa").toggleClass("fa-times");
-    $(this).parent().find(".fa").toggleClass("fa-check");
     $(this).parent().parent().toggleClass("check");
   });
 
