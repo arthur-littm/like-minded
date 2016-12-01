598b0f020ed5c0aab658404d0849d2ac6b05a050
Petya Nikolova <petia_182@yahoo.com>
2016-12-01 16:25:05 +0000

    changed button colors on focus and active - Preview and See answers

--- app/assets/javascripts/google_maps_autocomplete.js
+++ app/assets/javascripts/google_maps_autocomplete.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   }
 
   if (answer_location) {
-    var autocomplete = new google.maps.places.Autocomplete(answer_location, { types: ['geocode'] });
+    var autocomplete = new google.maps.places.Autocomplete(answer_location, { types: ['establishment'] });
     google.maps.event.addDomListener(answer_location, 'keydown', function(e) {
       if (e.keyCode == 13) {
         e.preventDefault(); // Do not submit the form on Enter.
