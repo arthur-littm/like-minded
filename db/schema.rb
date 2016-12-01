0f5dcd33f9b50129fdb4d0d5ca14b862b2c41d5c
Arthur Littmann <arthur.littmann@gmail.com>
2016-12-01 16:17:56 +0000

    picture fixed

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,20 @@ ActiveRecord::Schema.define(version: 20161201132741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.string   "author_type"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  end
+
   create_table "answers", force: :cascade do |t|
     t.string   "content"
     t.integer  "user_id"
@@ -93,18 +107,18 @@ ActiveRecord::Schema.define(version: 20161201132741) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "provider"
@@ -113,6 +127,7 @@ ActiveRecord::Schema.define(version: 20161201132741) do
     t.string   "token"
     t.datetime "token_expiry"
     t.string   "hometown"
+    t.boolean  "admin",                  default: false, null: false
     t.float    "latitude"
     t.float    "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
